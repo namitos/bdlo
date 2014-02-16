b73ac49a03cd2bcdfa32413689f29336ff11bb97
namit <namit@yandex.ru>
2014-02-16 18:38:28 +0400

    added separated worker file

--- app/app.js
+++ app/app.js
@@ -1,180 +1,71 @@
-var express = require('express.io');
-var app = express();
-var RedisStore = require('connect-redis')(express);
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mongodb = require('mongodb');
-var fs = require('fs');
-var logger = require('winston');
 var os = require('os');
+var fs = require('fs');
 var cluster = require('cluster');
+var exec = require("child_process").exec;
 var vow = require('vow');
 
-var conf = require('./conf');
-
-var sessionConfiguration={
-	cookieParser: express.cookieParser,
-	store: new RedisStore({ host: conf.session.redis.host, port: conf.session.redis.port, ttl: 604800 }),
-	secret: conf.session.secret,
-	key: 'session',
-	cookie: { maxAge: 604800000 },
-	fail:function(data, accept){
-		accept(null, true);
-	},
-	success:function(data, accept){
-		accept(null, true);
-	}
-};
-
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, { level: 'info', colorize: true, timestamp: true });
-logger.info('--------------------');
-logger.info('process.env.NODE_ENV', process.env.NODE_ENV);
-
-app.set('port', conf.port);
-app.set('views', conf.viewsDir);
-app.set('view cache', conf.viewCache);
-app.set('view engine', 'ejs');
-
-app.use(express.favicon());
-app.use(express.bodyParser());
-app.use(express.cookieParser());
-app.use(express.session(sessionConfiguration));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(express.static(__dirname + '/../static'));
-app.use(function (req, res, next) {
-	res.renderPage = function (template, parameters) {
-		res.render(template, parameters, function (err, html) {
-			res.render('page', {
-				html: html,
-				user: req.hasOwnProperty('user')?req.user:false
-			});
-		});
-	};
-	next();
-});
-app.use(function (req, res, next) {
-	var url=req.url.split('/');
-	if(url[1]=='admin'){
-		if(req.hasOwnProperty('user') && req.user.permission('full access')){
-			next();
-		}else{
-			res.send(403 ,'access denied');
-		}
-	}else{
-		next();
-	}
-});
-
-app.http().io();
-
-app.io.set('heartbeat timeout', 50);
-app.io.set('heartbeat interval', 20);
-app.io.set('browser client minification', true);
-app.io.set('store', new express.io.RedisStore(conf.ioStore));
-
-/*app.io.set('authorization', function (data, accept) {
-	require('passport.socketio').authorize(sessionConfiguration)(data, accept);
-});*/
-
-/*require('express.io-middleware')(app);
-app.io.use(function (req, next) {
-	console.log('middleware');
-	next();
-});*/
-
-logger.info('Initialized io');
-
-
-var mongoConnectPromise = function () {
-	return new vow.Promise(function(resolve, reject, notify) {
-		mongodb.MongoClient.connect(conf.mongoConnect, function (err, db) {
-			if (err) {
-				reject(err);
-			}
-			app.set('db', db);
-
-			//MMMMM, SPAGHETTI!
-			var User = require('./models/user');
-			passport.use(new LocalStrategy(
-				function (username, password, done) {
-					console.log('trying', username, password);
-					password = require('crypto').createHash('sha512').update(password).digest("hex");
-					db.collection('users').find({username: username, password:password}).toArray(function(err, result){
-						if(err){
-							done(err, null);
-						}else{
-							if(result.length){
-								console.log('user exists');
-								done(null, new User(result[0]));
-							}else{
-								console.log('user not exists');
-								done(null, null);
-							}
-						}
-					});
-				}
-			));
-			passport.serializeUser(function (user, done) {
-				console.log('user serialize', user);
-				done(null, user._id.toString());
-			});
-			passport.deserializeUser(function (id, done) {
-				console.log('user deserialize', id);
-				db.collection('users').find({_id: new mongodb.ObjectID(id)}).toArray(function(err, result){
-					if(err){
-						done(err, null);
-					}else{
-						if(result.length){
-							done(null, new User(result[0]));
-						}else{
-							console.log('user not exists');
-							done(null, null);
-						}
+function clearPids(callback) {
+	var killPromise = function (pid) {
+		return new vow.Promise(function(resolve, reject, notify) {
+			exec('kill ' + pid, function (error, stdout, stderr) {
+				console.log('kill ' + pid, error, stdout, stderr);
+				fs.unlink('./pids/' + pid, function (err) {
+					console.log('delete ' + pid);
+					resolve();
+					if (err) {
+						console.log(err);
 					}
 				});
 			});
-
-			logger.info('Mongo connected');
-			resolve();
 		});
-	});
-};
-
-
-var routesPromise = function () {
-	return new vow.Promise(function(resolve, reject, notify){
-		fs.readdir('./routes', function (err, files) {
-			if (err) {
-				reject(err);
-			}
-			files.forEach(function (file) {
-				require('./routes/' + file).init(app);
+	};
+	fs.readdir('./pids', function (err, files) {
+		if (err) {
+			console.log(err);
+			fs.mkdir('pids', function(){
+				callback();
 			});
-			logger.info('Routes loaded');
-			resolve();
-		});
+		}else{
+			var promises = [];
+			files.forEach(function (file) {
+				if(file!='donotdelete'){
+					promises.push(killPromise(file));
+				}
+			});
+			vow.all(promises).then(function () {
+				callback();
+			});
+		}
 	});
-};
 
+}
 
-vow.all([mongoConnectPromise(), routesPromise()]).then(function (results) {
-	if (cluster.isMaster) {
+if (cluster.isMaster) {
+	clearPids(function () {
+		fs.writeFile('./pids/' + process.pid, 'master', function (err) {});
 		var CPUsCount = os.cpus().length;
 		for (var i = 0; i < CPUsCount; ++i) {
 			cluster.fork();
 		}
-		cluster.on('listening', function (worker, address) {
-			logger.info('Worker %d is now listening on port %d in %s mode.', worker.process.pid, address.port,
-				app.get('env'));
-		});
 		cluster.on('exit', function (worker) {
-			logger.info('Worker %d died.', worker.process.pid);
+			fs.unlink('./pids/' + worker.process.pid, function (err) {});
+			console.log('Worker ' + worker.process.pid + ' died.');
 			cluster.fork();
 		});
-	} else {
-		app.listen(app.get('port'));
-	}
-});
+
+		cluster.on('listening', function (worker, address) {
+			fs.writeFile('./pids/' + worker.process.pid, '', function (err) {});
+			console.log('Worker ' + worker.process.pid + ' is now listening on port ' + address.port + ' in ' + process.env.NODE_ENV + ' mode.');
+			worker.on('message', function (msg) {
+				if(msg.cmd=='restart'){
+					console.log('restart all children instances');
+					Object.keys(cluster.workers).forEach(function(id) {
+						cluster.workers[id].kill();
+					});
+				}
+			});
+		});
+	});
+} else {
+	require('./worker')();
+}
---
+++ app/worker.js
@@ -0,0 +1,186 @@
+module.exports = function (callback) {
+	var express = require('express.io');
+	var app = express();
+	var RedisStore = require('connect-redis')(express);
+	var passport = require('passport');
+	var LocalStrategy = require('passport-local').Strategy;
+	var mongodb = require('mongodb');
+	var fs = require('fs');
+//var logger = require('winston');
+	var vow = require('vow');
+//var vowFs = require('vow-fs');
+	var drev = require('drev');
+
+	var conf = require('./conf');
+
+	var sessionConfiguration = {
+		cookieParser: express.cookieParser,
+		store: new RedisStore({ host: conf.session.redis.host, port: conf.session.redis.port, ttl: 604800 }),
+		secret: conf.session.secret,
+		key: 'session',
+		cookie: { maxAge: 604800000 },
+		fail: function (data, accept) {
+			accept(null, true);
+		},
+		success: function (data, accept) {
+			accept(null, true);
+		}
+	};
+
+//logger.remove(logger.transports.Console);
+//logger.add(logger.transports.Console, { level: 'info', colorize: true, timestamp: true });
+//logger.info('--------------------');
+//console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
+	app.set('env', process.env.hasOwnProperty('NODE_ENV') ? process.env.NODE_ENV : 'production');
+	app.set('port', conf.port);
+	app.set('views', conf.viewsDir);
+	app.set('view cache', conf.viewCache);
+	app.set('view engine', 'ejs');
+
+
+	app.use(express.favicon());
+
+	app.use(express.bodyParser({ limit: '500mb'}));
+//app.use(express.json({limit: '500mb'}));
+//app.use(express.urlencoded({limit: '500mb'}));
+
+	app.use(express.cookieParser());
+	app.use(express.session(sessionConfiguration));
+	app.use(passport.initialize());
+	app.use(passport.session());
+
+	app.use(express.static(__dirname + '/../static'));
+
+	app.use(function (req, res, next) {
+		var url = req.url.split('/');
+		res.renderPage = function (template, parameters) {
+			res.render(template, parameters, function (err, html) {
+				res.render(url[1] == 'admin' ? 'admin/page' : 'page', {
+					html: html,
+					user: req.hasOwnProperty('user')?req.user:false
+				});
+			});
+		};
+		next();
+	});
+	app.use(function (req, res, next) {
+		var url = req.url.split('/');
+		if (url[1] == 'admin') {
+			if (req.hasOwnProperty('user') && req.user.permission('full access')) {
+				next();
+			} else {
+				res.send(403, 'access denied');
+			}
+		} else {
+			next();
+		}
+	});
+
+	app.http().io();
+
+	app.io.set('heartbeat timeout', 50);
+	app.io.set('heartbeat interval', 20);
+	app.io.set('browser client minification', true);
+	app.io.set('store', new express.io.RedisStore(conf.ioStore));
+
+
+	app.io.set('authorization', function (data, accept) {
+		//console.log(data);
+		//console.log(require('passport.socketio').authorize(sessionConfiguration).toString());
+		//logger.info('New client (%s) connecting in common mode.', data.headers['x-real-ip']);
+		require('passport.socketio').authorize(sessionConfiguration)(data, accept);
+	});
+
+	/*app.io.on("connection", function(socket){
+	 console.log(socket.handshake.user);
+	 });*/
+
+	require('express.io-middleware')(app);
+	app.io.use(function (req, next) {
+		console.log(req.handshake.user);
+		console.log('middleware!!!!!!!!!!!!!');
+		next();
+	});
+
+	function mongoConnectPromise() {
+		return new vow.Promise(function (resolve, reject, notify) {
+			var MongoClient = mongodb.MongoClient;
+			MongoClient.connect(conf.mongoConnect, function (err, db) {
+				if (err) {
+					throw err;
+				}
+				app.set('db', db);
+
+				var User = require('./models/user');
+				passport.use(new LocalStrategy(
+					function (username, password, done) {
+						console.log('trying', username, password);
+						password = require('crypto').createHash('sha512').update(password).digest("hex");
+						db.collection('users').find({username: username, password: password}).toArray(function (err, result) {
+							if (err) {
+								done(err, null);
+							} else {
+								if (result.length) {
+									console.log('user exists');
+									done(null, new User(result[0]));
+								} else {
+									console.log('user not exists');
+									done(null, null);
+								}
+							}
+						});
+					}
+				));
+				passport.serializeUser(function (user, done) {
+					console.log('user serialize', user);
+					done(null, user._id.toString());
+				});
+				passport.deserializeUser(function (id, done) {
+					console.log('user deserialize', id);
+					db.collection('users').find({_id: new mongodb.ObjectID(id)}).toArray(function (err, result) {
+						if (err) {
+							done(err, null);
+						} else {
+							if (result.length) {
+								done(null, new User(result[0]));
+							} else {
+								console.log('user not exists');
+								done(null, null);
+							}
+						}
+					});
+				});
+				//logger.info('Mongo connected');
+				resolve();
+			});
+		});
+	}
+
+	function routesPromise() {
+		return new vow.Promise(function (resolve, reject, notify) {
+			fs.readdir('./routes', function (err, files) {
+				if (err) {
+					throw err;
+				}
+				files.forEach(function (file) {
+					require('./routes/' + file).init(app);
+				});
+				resolve();
+			});
+		});
+	}
+
+
+	vow.all({
+		mongo: mongoConnectPromise(),
+		routes: routesPromise()
+	}).then(function (result) {
+			drev.start(conf.session.redis.host, conf.session.redis.port);
+			app.listen(app.get('port'));
+			if (callback) {
+				callback(result);
+			}
+		});
+};
+
