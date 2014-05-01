75d055e95860554b54c18603ced3ebcf52d16e73
namit <namit@yandex.ru>
2014-05-01 19:31:15 +0400

    updated load()

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,12 +1,18 @@
 /** Асинхронный подгрузчик всякого *********************************************/
 var load = function (urls, onLoad) {
 	var loadPromise = function (url) {
-		return new vow.Promise(function(resolve, reject, notify) {
+		return new vow.Promise(function (resolve, reject, notify) {
 			$.get(url, function (data) {
 				var ext = url.split('.');
-				ext = ext[ext.length-1];
-				if(ext == 'ejs'){
+				ext = ext[ext.length - 1];
+				if (ext == 'ejs') {
 					data = _.template(data);
+				} else if (ext == 'js') {
+					var module = {};
+					(function (module) {//это сделано для схем, которые также подтягиваются на бекенде и пишутся в module.exports
+						eval(data);
+					})(module);
+					data = module.exports;
 				}
 				resolve(data);
 			});
