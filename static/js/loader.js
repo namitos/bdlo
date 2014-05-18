4ad26708063fbe6bafa95dbf7cceedb6e9a76f75
Artem <namit@yandex.ru>
2014-05-18 22:43:19 +0400

    Update loader.js

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,7 +1,15 @@
 /** Асинхронный подгрузчик всякого *********************************************/
 var load = function (urls, onLoad) {
-	var loadPromise = function (url) {
+	var loadPromise = function (input) {
 		return new vow.Promise(function (resolve, reject, notify) {
+			var traverse = {};
+			var url;
+			if (typeof input == 'string') {
+				url = input;
+			} else {
+				traverse = input;
+				url = traverse.url;
+			}
 			$.get(url, function (data) {
 				var ext = url.split('.');
 				ext = ext[ext.length - 1];
@@ -14,7 +22,14 @@ var load = function (urls, onLoad) {
 					})(module);
 					data = module.exports;
 				}
-				resolve(data);
+				var toResolve;
+				if (traverse.hasOwnProperty('url')) {
+					toResolve = traverse;
+					toResolve.result = data;
+				} else {
+					toResolve = data;
+				}
+				resolve(toResolve);
 			});
 		});
 	};
