67ae38cdb7cf147a7c44c8c469ce8b8209a567c9
namit <namit@yandex.ru>
2014-05-22 10:33:36 +0400

    added route where developer may get user profile

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,18 +1,31 @@
 var passport = require('passport');
 
-module.exports = function(app){
-	app.get('/logout', function(req, res){
+module.exports = function (app) {
+	app.get('/logout', function (req, res) {
 		req.logout();
 		res.redirect('/');
 	});
-	app.get('/login', function(req, res){
-		if(req.hasOwnProperty('user')){
+
+	app.get('/login', function (req, res) {
+		if (req.hasOwnProperty('user')) {
 			res.redirect('/');
-		}else{
+		} else {
 			res.renderPage(app.get('adminViewsPath') + '/userlogin');
 		}
 	});
-	app.post('/login', passport.authenticate('local', { failureRedirect: '/login?err=1' }), function(req, res){
+
+	app.post('/login', passport.authenticate('local', { failureRedirect: '/login?err=1' }), function (req, res) {
 		res.redirect('/');
 	});
+
+	app.get('/userscript', function (req, res) {
+		var user = {};
+		if (req.hasOwnProperty('user')) {
+			var user = req.user;
+			delete user.password;
+		}
+		res.header({'Content-Type': 'application/javascript'});
+		res.send("var user = new User(" + JSON.stringify(user) + "); try{ module.exports = user; } catch(e) {}");
+	});
+
 };
