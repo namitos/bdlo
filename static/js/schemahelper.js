0c3f33709ffb2cd554c8a9b19e68f30495864c81
Artem <namit@yandex.ru>
2014-05-30 05:58:30 -0400

    Update schemahelper.js

--- static/js/schemahelper.js
+++ static/js/schemahelper.js
@@ -1,5 +1,5 @@
 /** Помощь по схемам и генератор форм из них *********************************************/
-(function(context){
+(function (context) {
 	function formPartPrimitive(fieldSchema, value, name) {
 		var inputTypes = {
 			string: 'text',
@@ -13,67 +13,70 @@
 			'class': 'form-control',
 			required: fieldSchema.hasOwnProperty('required') && fieldSchema.required == true ? true : false
 		};
-		var input='';
-		var label=name;
-		if(fieldSchema.hasOwnProperty('info')){
-			if(fieldSchema.info.hasOwnProperty('label')){
-				label=fieldSchema.info.label;
+		var input = '';
+		var label = name;
+		if (fieldSchema.hasOwnProperty('info')) {
+			if (fieldSchema.info.hasOwnProperty('label')) {
+				label = fieldSchema.info.label;
 			}
-			if(fieldSchema.info.hasOwnProperty('placeholder')){
-				attributes.placeholder=fieldSchema.info.placeholder;
+			if (fieldSchema.info.hasOwnProperty('placeholder')) {
+				attributes.placeholder = fieldSchema.info.placeholder;
 			}
-			if(fieldSchema.info.hasOwnProperty('pattern')){
-				attributes.pattern=fieldSchema.info.pattern;
+			if (fieldSchema.info.hasOwnProperty('pattern')) {
+				attributes.pattern = fieldSchema.info.pattern;
 			}
-			if(fieldSchema.info.hasOwnProperty('multiple')){
-				attributes.multiple=fieldSchema.info.multiple;
+			if (fieldSchema.info.hasOwnProperty('multiple')) {
+				attributes.multiple = fieldSchema.info.multiple;
 			}
 		}
-		if(fieldSchema.hasOwnProperty('minimum')){
+		if (fieldSchema.hasOwnProperty('minimum')) {
 			attributes.min = fieldSchema.minimum;
 		}
 
 
 		if (fieldSchema.hasOwnProperty('info') && fieldSchema.info.hasOwnProperty('type')) {
 			attributes.type = fieldSchema.info.type;
-		}else{
+		} else {
 			attributes.type = inputTypes[fieldSchema.type];
 		}
 
-		if(attributes.type=='file' && value && value.length){
-			input+="<ul class='file-list'>";
-			var fileName='';
-			for(var i=0;i<value.length;++i){
-				if(value[i]!=null){
-					fileName=value[i].split('/');
-					fileName=fileName[fileName.length-1];
-					input+="<li data-i='"+i+"' data-field='"+name+"'><a class='btn-file-delete'><span class='glyphicon glyphicon-remove'></span><span class='text'>Delete</span></a><a style='display:inline-block;width:100px;overflow:hidden;text-overflow:ellipsis;' target='_blank' href='/"+value[i]+"'>"+fileName+"</a></li>";
+		if (attributes.type == 'file' && value && value.length) {
+			input += "<ul class='file-list'>";
+			var fileName = '';
+			for (var i = 0; i < value.length; ++i) {
+				if (value[i] != null) {
+					fileName = value[i].split('/');
+					fileName = fileName[fileName.length - 1];
+					input += "<li data-i='" + i + "' data-field='" + name + "'><a class='btn-file-delete'><span class='glyphicon glyphicon-remove'></span><span class='text'>Delete</span></a><a style='display:inline-block;width:100px;overflow:hidden;text-overflow:ellipsis;' target='_blank' href='/" + value[i] + "'>" + fileName + "</a></li>";
 				}
 			}
-			input+="</ul>";
+			input += "</ul>";
 		}
-		if(attributes.type=='select'){
-			input+="<select " + htmlAttributes(attributes) + "><option></option>";
-			for(var key in fieldSchema.info.options){
-				input+="<option value='"+key+"' "+(key==attributes.value?'selected':'')+">"+fieldSchema.info.options[key]+"</option>";
+		if (attributes.type == 'select') {
+			input += "<select " + htmlAttributes(attributes) + "><option></option>";
+			for (var key in fieldSchema.info.options) {
+				input += "<option value='" + key + "' " + (key == attributes.value ? 'selected' : '') + ">" + fieldSchema.info.options[key] + "</option>";
 			}
-			input+="</select>";
-		}else if(attributes.type=='textarea'){
+			input += "</select>";
+		} else if (attributes.type == 'textarea') {
 			var value = attributes.value;
 			delete attributes.value;
-			input+="<textarea " + htmlAttributes(attributes) + ">" + value + "</textarea>";
-		}else{
-			input+="<input " + htmlAttributes(attributes) + ">";
+			input += "<textarea " + htmlAttributes(attributes) + ">" + value + "</textarea>";
+		} else {
+			input += "<input " + htmlAttributes(attributes) + ">";
 		}
 
-		return "<div class='form-group'><label>" + label + "</label>"+input+"</div>";
+		return "<div class='form-group input-" + name.replace('[', '').replace(']', '').replace('.', '') + "'><label>" + label + "</label>" + input + "</div>";
 	}
-	function formPartMulti(schema, obj, parentKeyName, i){
-		return "<div class='multi-item' data-i='"+i+"' data-field='"+parentKeyName+"'>"+formPart(schema, obj, parentKeyName+'['+i+']')+"<button class='btn btn-danger btn-multi-delete'><span class='glyphicon glyphicon-minus'></span><span class='text'>Delete</span></button></div>";
+
+	function formPartMulti(schema, obj, parentKeyName, i) {
+		return "<div class='multi-item' data-i='" + i + "' data-field='" + parentKeyName + "'>" + formPart(schema, obj, parentKeyName + '[' + i + ']') + "<button class='btn btn-danger btn-multi-delete'><span class='glyphicon glyphicon-minus'></span><span class='text'>Delete</span></button></div>";
 	}
-	function formPartMultiPrimitive(schema, obj, parentKeyName, i){
-		return "<div class='multi-item' data-i='"+i+"' data-field='"+parentKeyName+"'>"+formPartPrimitive(schema, obj, parentKeyName+'['+i+']')+"<button class='btn btn-danger btn-multi-delete'><span class='glyphicon glyphicon-minus'></span><span class='text'>Delete</span></button></div>";
+
+	function formPartMultiPrimitive(schema, obj, parentKeyName, i) {
+		return "<div class='multi-item' data-i='" + i + "' data-field='" + parentKeyName + "'>" + formPartPrimitive(schema, obj, parentKeyName + '[' + i + ']') + "<button class='btn btn-danger btn-multi-delete'><span class='glyphicon glyphicon-minus'></span><span class='text'>Delete</span></button></div>";
 	}
+
 	function formPart(schema, obj, parentKeyName) {
 		var html = '';
 		if (!obj) {
@@ -93,8 +96,8 @@
 			} else if (fieldSchema.type == 'array') {
 				keyName = parentKeyName ? parentKeyName + '.' + key : key;
 				html += "<fieldset class='multi'>";
-				if(fieldSchema.hasOwnProperty('info') && fieldSchema.info.hasOwnProperty('label')){
-					html +=	"<legend>" + fieldSchema.info.label + "</legend>";
+				if (fieldSchema.hasOwnProperty('info') && fieldSchema.info.hasOwnProperty('label')) {
+					html += "<legend>" + fieldSchema.info.label + "</legend>";
 				}
 
 				var i = 0;
@@ -116,83 +119,86 @@
 				html += formPartPrimitive(fieldSchema, obj.hasOwnProperty(key) ? obj[key] : '', keyName);
 			}
 		}
-		if(schema.hasOwnProperty('info') && schema.info.hasOwnProperty('label')){
-			html +=	"<legend>" + schema.info.label + "</legend>";
+		if (schema.hasOwnProperty('info') && schema.info.hasOwnProperty('label')) {
+			html += "<legend>" + schema.info.label + "</legend>";
 		}
-		return "<fieldset>" + html + "</fieldset>";
+		return "<fieldset class='group-" + parentKeyName + "'>" + html + "</fieldset>";
 	}
-	function form(schema, obj, formAttributes){
+
+	function form(schema, obj, formAttributes) {
 		var $form = $("<form " + htmlAttributes(formAttributes) + "></form>");
 		$form.append(formPart(schema, obj));
-		if(formAttributes){
-			if(formAttributes.hasOwnProperty('method')){
+		if (formAttributes) {
+			if (formAttributes.hasOwnProperty('method')) {
 				$form.append("<div class='form-group'><input type='submit' class='btn btn-primary'></div>");
-			}else{
-				$form.on('submit', function(){
+			} else {
+				$form.on('submit', function () {
 					return false;
 				});
 			}
 		}
 		return $form;
 	}
-	function htmlAttributes(attributes){
-		var attributesArray=[];
-		for(var key in attributes){
-			if(attributes[key]===false){
 
-			} else if(attributes[key]===true){
+	function htmlAttributes(attributes) {
+		var attributesArray = [];
+		for (var key in attributes) {
+			if (attributes[key] === false) {
+
+			} else if (attributes[key] === true) {
 				attributesArray.push(key);
-			}else{
-				attributesArray.push(key+"='"+attributes[key]+"'");
+			} else {
+				attributesArray.push(key + "='" + attributes[key] + "'");
 			}
 		}
 		return attributesArray.join(' ');
 	}
-	function generate(obj){
-		var schema={};
-		if(obj instanceof Array){
-			schema.type='array';
-			schema.items=generate(obj[0]);
 
-		}else if(obj instanceof Object){
-			schema.type='object';
-			schema.properties={};
-			for(var key in obj){
-				schema.properties[key]=generate(obj[key]);
+	function generate(obj) {
+		var schema = {};
+		if (obj instanceof Array) {
+			schema.type = 'array';
+			schema.items = generate(obj[0]);
+
+		} else if (obj instanceof Object) {
+			schema.type = 'object';
+			schema.properties = {};
+			for (var key in obj) {
+				schema.properties[key] = generate(obj[key]);
 			}
-		}else if(typeof obj == 'string'){
-			schema.type='string';
+		} else if (typeof obj == 'string') {
+			schema.type = 'string';
 
-		}else if(typeof obj == 'number'){
-			schema.type='number';
+		} else if (typeof obj == 'number') {
+			schema.type = 'number';
 
 		}
 		return schema;
 	}
 
-	$(document).on('click', '.btn-multi-add', function(e){
-		var $el=jQuery(this);
-		var schema=$el.data('schema');
-		var keyName=$el.data('keyname');
+	$(document).on('click', '.btn-multi-add', function (e) {
+		var $el = jQuery(this);
+		var schema = $el.data('schema');
+		var keyName = $el.data('keyname');
 		var i = $el.data('i');
-		if(schema.type != 'object' && schema.type != 'array'){
+		if (schema.type != 'object' && schema.type != 'array') {
 			$el.before(formPartMultiPrimitive(schema, '', keyName, i));
-		}else{
+		} else {
 			$el.before(formPartMulti(schema, {}, keyName, i));
 		}
 
 		$el.data('i', ++i);
 		e.preventDefault();
 	});
-	$(document).on('click', '.btn-multi-delete', function(e){
-		var $el=jQuery(this);
-		var $row=$el.parent();
+	$(document).on('click', '.btn-multi-delete', function (e) {
+		var $el = jQuery(this);
+		var $row = $el.parent();
 		$row.remove();
 		e.preventDefault();
 	});
-	$(document).on('click', '.btn-file-delete', function(e){
-		var $el=jQuery(this);
-		var $row=$el.parent();
+	$(document).on('click', '.btn-file-delete', function (e) {
+		var $el = jQuery(this);
+		var $row = $el.parent();
 		$row.remove();
 		e.preventDefault();
 	});
