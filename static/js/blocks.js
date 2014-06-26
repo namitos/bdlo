a32faf6c79d5cef32bf379e935c0bb4f23e2e0fb
Artem <namit@yandex.ru>
2014-06-26 17:09:07 +0400

    Update blocks.js

--- static/js/blocks.js
+++ static/js/blocks.js
@@ -14,7 +14,7 @@
 			});
 			blocks.forEach(function (block) {
 				var $wrapper = $("<div class='panel panel-default panel-" + block._id + "'><div class='panel-heading'><div class='panel-title'>" + block.title + "</div></div><div class='panel-body'>" + block.content + "</div></div>");
-				if (block.hasOwnProperty('widget')) {
+				if (block.hasOwnProperty('widget') && block.widget) {
 					$wrapper.addClass('panel-' + block.widget);
 					$wrapper.find('.panel-body').html(widgets[block.widget]());
 				}
