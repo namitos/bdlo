de974ddf02383f868472a8b11a772e3119521392
Artem <namitos@users.noreply.github.com>
2015-02-26 10:20:58 +0300

    Update users.js

--- static/schemas/users.js
+++ static/schemas/users.js
@@ -14,23 +14,9 @@ var schema = {
 			items: {
 				type: 'string'
 			}
-		},
-		avatar: {
-			type: 'any',//для схемы мы указали тип любой, чтобы потом в info его переопределить как file
-			info: {
-				type: 'file',
-				mimes: ['image/jpeg', 'image/png'],//какие mime типы разрешены
-				storage: {//инфа о хранилище
-					access: 'pub',//доступ публичный
-					type: 'filesystem',//в файловой системе
-					path: 'avatars'//папка в этом хранилище
-				}
-			}
 		}
 	},
-	info: {
-		ownerField: '_id',
-	},
+	ownerField: '_id',
 	titleField: 'username',
 	name: 'Users'
 };
