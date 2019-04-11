b3a811bff1bf32322fd523d51074e6e236d8f00f
Artem <2332720+namitos@users.noreply.github.com>
2019-04-11 13:53:42 +0300

    Update imgresize.js

--- middleware/imgresize.js
+++ middleware/imgresize.js
@@ -24,64 +24,50 @@
 //   }
 // }
 
-let path = require('path');
-let fs = require('fs');
-
-let gm = require('gm');
-let mkdirp = require('mkdirp');
+const path = require('path');
+const fs = require('fs');
+const gm = require('gm');
+const mkdirp = require('mkdirp');
+const util = require('util');
 
 module.exports = (settings) => {
   settings.quality = settings.quality || 80;
-
-  return (req, res, next) => {
-    let file = req.url.replace(/\?.*/, '');
-    let origin = path.join(settings.path, file);
-    let profileName = req.query.profile || 'thumb';
-    console.log('resizing', settings.path, file, origin, profileName);
-
-    if (settings.profile[profileName]) {
-      let profile = settings.profile[profileName];
-      fs.stat(origin, (err, result) => {
-        if (err) {
-          res.status(404).send();
-        } else {
-          let resizedFilePath = path.join(settings.path, settings.tmp, profileName, file);
-          fs.stat(resizedFilePath, (err, result) => {
-            if (err) {
-              mkdirp(path.dirname(resizedFilePath), (err) => {
-                let image = gm(origin).quality(settings.quality);
-                image.size(function(err, size) {
-                  if (err) {
-                    res.status(503).send();
-                  } else {
-                    if (size.width > profile.w || size.height > profile.h) {
-                      image.resize(profile.w, profile.h).write(resizedFilePath, (err) => {
-                        if (err) {
-                          res.status(503).send();
-                        } else {
-                          res.redirect(path.join(settings.tmp, profileName, file));
-                        }
-                      });
-                    } else {
-                      image.resize(size.width, size.height).write(resizedFilePath, (err) => {
-                        if (err) {
-                          res.status(503).send();
-                        } else {
-                          res.redirect(path.join(settings.tmp, profileName, file));
-                        }
-                      });
-                    }
-                  }
-                });
-              });
-            } else {
-              res.redirect(path.join(settings.tmp, profileName, file));
-            }
-          });
+  return async (req, res, next) => {
+    try {
+      let file = req.url.replace(/\?.*/, '');
+      file = decodeURIComponent(file);
+      let origin = path.join(settings.fsPath, file);
+      let profileName = req.query.profile || 'thumb';
+      //console.log('resizing', settings.fsPath, file, origin, profileName);
+      if (settings.profile[profileName]) {
+        let profile = settings.profile[profileName];
+        try {
+          await util.promisify(fs.stat)(origin);
+        } catch (err) {
+          return res.status(404).send();
         }
-      });
-    } else {
-      res.status(404).send();
+        let resizedFilePath = path.join(settings.fsPath, settings.tmp, profileName, file);
+        try {
+          await util.promisify(fs.stat)(resizedFilePath);
+          return res.redirect(path.join(settings.redirPath, settings.tmp, profileName, file));
+        } catch (err) {
+          await util.promisify(mkdirp)(path.dirname(resizedFilePath));
+          let image = gm(origin).quality(settings.quality);
+          let size = await util.promisify(image.size).bind(image)();
+          if (size.width > profile.w || size.height > profile.h) {
+            await util.promisify(image.resize(profile.w, profile.h).write).bind(image)(resizedFilePath);
+            return res.redirect(path.join(settings.redirPath, settings.tmp, profileName, file));
+          } else {
+            await util.promisify(image.resize(size.width, size.height).write).bind(image)(resizedFilePath);
+            return res.redirect(path.join(settings.redirPath, settings.tmp, profileName, file));
+          }
+        }
+      } else {
+        res.status(404).send();
+      }
+    } catch (err) {
+      console.error(err)
+      res.status(503).send();
     }
-  };
+  }
 };
